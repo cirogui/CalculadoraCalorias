8211c824b7863ba5b622f2041f981e1a6f8c7fc5
cirogui <ciro.guilherme@hotmail.com>
2022-06-13 13:16:14 -0300

    comment

--- calc.go
+++ calc.go
@@ -14,34 +14,38 @@ func init() {
 }
 
 func main() {
-	http.HandleFunc("/", index)
-	http.HandleFunc("/process", processor)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", index)            //ir para o diretório do template
+	http.HandleFunc("/process", processor) //tratar a requisição http
+	http.ListenAndServe(":8080", nil)      //criar servidor http local
 
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	tpl.ExecuteTemplate(w, "index.gohtml", nil) //executar o template
 }
 
 func processor(w http.ResponseWriter, r *http.Request) {
+	//se o método utilizado não for post, retorna
 	if r.Method != "POST" {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
 
+	//cada variável vai receber os dados dos formulários em formato string
 	Peso := r.FormValue("Peso")
 	Sexo := r.FormValue("Sexo")
 	Altura := r.FormValue("Altura")
 	Idade := r.FormValue("Idade")
 	FatorAtividade := r.FormValue("Fator de Atividade")
 
+	//variáveis para converter string para float
 	intPeso, err := strconv.ParseFloat(Peso, 32)
-	fmt.Println(intPeso, err)
+	fmt.Println(err)
 	intAltura, err := strconv.ParseFloat(Altura, 32)
 	intIdade, err := strconv.ParseFloat(Idade, 32)
 	intFatorAtividade, err := strconv.ParseFloat(FatorAtividade, 32)
 
+	//fórmula Método Harris-Benedict já com os dados em float para serem calculados
 	FormulaMasc := 66 + (13.7 * intPeso) + (5 * intAltura) - (6.8 * intIdade)
 	FormulaFem := 655 + (9.6 * intPeso) + (1.8 * intAltura) - (4.7 * intIdade)
 
